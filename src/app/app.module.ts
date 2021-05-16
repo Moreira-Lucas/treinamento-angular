d482c24d1538da5ed2b2ce08997a3994c3a251d3
lucg23 <lucasferreira.23m@gmail.com>
2021-05-16 00:13:34 -0300

    criando página 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,33 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './stars/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path:'courses',
+        component:CourseListComponent
+      },
+      {
+        path:'**', component:
+      }
+    ])
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -5,7 +5,7 @@ import { CouserService } from "./course.service";
 
 
 @Component({
-  selector:'app-course-list',
+  
   templateUrl:'./course-list.component.html'
 })
 
